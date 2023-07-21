afc0ae8e98c2284a14e836db3bd7e27ebc4dc836
Harsh Khulbe <harshkhulbe18@gmail.com>
2023-07-21 17:14:29 +0530

    Added airport seeders

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { PORT } = require("./config/serverConfig");
 const ApiRoutes = require("./routes/index");
+const db = require("./models/index");
 
 const setupAndStartServer = async () => {
   const app = express();
@@ -10,6 +11,9 @@ const setupAndStartServer = async () => {
   app.use("/api", ApiRoutes);
   app.listen(PORT, async () => {
     console.log(`Server started at ${PORT}`);
+    if (process.env.SYNC_DB) {
+      db.sequelize.sync({ alter: true });
+    }
   });
 };
 
